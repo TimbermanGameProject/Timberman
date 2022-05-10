e9e44b23b97ad0ef912c56b78c87031952fda332
Adam <235885@edu.p.lodz.pl>
2022-05-10 13:37:05 +0200

    added styling to optionsWindow, values are reset when scenes are changed

--- src/main/java/options/window/OptionsWindowController.java
+++ src/main/java/options/window/OptionsWindowController.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import start.window.StartWindow;
 
@@ -12,7 +14,16 @@ import java.io.IOException;
 public class OptionsWindowController {
 
     @FXML
+    private CheckBox musicCheckbox;
+
+    @FXML
+    private TextField playersNumberBox;
+
+    @FXML
     private Button returnButton;
+
+    @FXML
+    private TextField roundTimeBox;
 
     @FXML
     void returnToStartWindow(ActionEvent event) throws IOException {
